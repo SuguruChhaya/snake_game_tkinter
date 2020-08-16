c70e30e743adda6ad45a5e713c4529daf0d15792
Suguru Chhaya <suguruchhaya@gmail.com>
2020-08-16 13:03:14 -0400

    have to improve movement

--- basicmovement.py
+++ basicmovement.py
@@ -3,6 +3,7 @@ import random
 class Snake():
     
     def __init__(self):
+        print('init is ran')
         self.root = Tk()
         Snake.my_canvas = Canvas(self.root, height=375, width=375, bg='black')
         Snake.my_canvas.grid(row=0, column=0)
@@ -15,7 +16,8 @@ class Snake():
         for width in range(0, 375, 25):
             Snake.my_canvas.create_line(width, 0, width, 500, fill='white')
 
-        self.item = Snake.my_canvas.create_rectangle(225, 225, 250, 250, fill='green', tags='item')
+        self.head = Snake.my_canvas.create_rectangle(225, 225, 250, 250, fill='light green', tags='item')
+        self.snake_body = [self.head]
         #*Trying to move this thing.
         #https://stackoverflow.com/questions/15269682/python-tkinter-canvas-fail-to-bind-keyboard
         self.up_bind = self.root.bind("<w>", lambda event: self.up(event))
@@ -25,50 +27,147 @@ class Snake():
         #*Default movement is up
         self.x = 0
         self.y = -25
+        self.snake_direction = 'up'
+        self.last_direction = 'up'
+        #*I am going to store the turns in the form of a nested list.
+        #*The first four floats will be the coordinates where I have to turn.
+        #*The fifth string will be the direction I need to turn to.
+        self.turns = []
         #*Just a variable to allow wall passing or no.
-        self.wall_death = True
+        self.wall_death = False
 
     #A great reference for controlling the speed of the moving boxes.
 
     def movement(self):
         #*Since this is the function that will constantly be running, I will want to add my checking functions here.
-        Snake.my_canvas.move(self.item, self.x, self.y)
+        #?So much to debug here
+        if self.snake_direction == self.last_direction:
+            #*I will first check whether there are any items in the self.turns list I have to catch up on.
+            if len(self.turns) == 0:
+                for item in self.snake_body:
+                    Snake.my_canvas.move(item, self.x, self.y)
+            
+            #*The case in which the head has turned, but the body hasn't kept up.
+            else:
+                #*I think I have to keep the head and all the already turned body parts separate in a list.
+                #*These parts have to be treated separately.
+                Snake.my_canvas.move(self.head, self.x, self.y)
+                for part in self.snake_body:
+                    #*if coordinate has matched
+                    if Snake.my_canvas.coords(part) == self.turns[0][0]:
+                        if self.snake_direction == 'up':
+                            Snake.my_canvas.move(part, 0, -25)
+                        elif self.snake_direction == 'down':
+                            Snake.my_canvas.move(part, 0, 25)
+                        elif self.snake_direction == 'left':
+                            Snake.my_canvas.move(part, -25, 0)
+                        elif self.snake_direction == 'right':
+                            Snake.my_canvas.move(part, 25,0)
+                    #*The rest of the snake which doesn't turn yet
+                    else:
+                        if self.turns[0][1] == 'up':
+                            Snake.my_canvas.move(part, 0, -25)
+                        elif self.turns[0][1] == 'down':
+                            Snake.my_canvas.move(part, 0, 25)
+                        elif self.turns[0][1] == 'left':
+                            Snake.my_canvas.move(part, -25, 0)
+                        elif self.turns[0][1] == 'right':
+                            Snake.my_canvas.move(part, 25,0)
+                self.turns = self.turns[1:]
+                
+        
+        else:
+            print('turned')
+            #*I will obviously have to get the coords of the head and check add the turning point
+            #*Temporary variable to store list
+            turn_add = []
+            turn_add.append(Snake.my_canvas.coords(self.head))
+            turn_add.append(self.snake_direction)
+            self.turns.append(turn_add)
+            print(self.turns)
+            Snake.my_canvas.move(self.head, self.x, self.y)
 
-        print(Snake.my_canvas.coords(self.item))
+        self.snake_x1 = Snake.my_canvas.coords(self.head)[0]
+        self.snake_y1 = Snake.my_canvas.coords(self.head)[1]
+        self.snake_x2 = Snake.my_canvas.coords(self.head)[2]
+        self.snake_y2 = Snake.my_canvas.coords(self.head)[3] 
+
+        print(Snake.my_canvas.coords(self.head))
         print(Snake.my_canvas.coords(Apple.apple_1))
 
         #*I should definitely make a variable to manage whether the after function will run next time or not.
         self.after_var = True
-        if Snake.my_canvas.coords(self.item) == Snake.my_canvas.coords(Apple.apple_1):
-            Snake.my_canvas.after_cancel(self.alive)
-            print('equal')
+        if Snake.my_canvas.coords(self.head) == Snake.my_canvas.coords(Apple.apple_1):
+            self.add_length()
+            Snake.my_canvas.delete(Apple.apple_1)
+            Apple.apple_count -= 1
+            self.after_var = True
+
+        #*Check if there are constantly 1 or more apples
+        if Apple.apple_count == 0:
+            b.create_apple()
         
         #*If hit the wall, freeze.
-        self.snake_x1 = Snake.my_canvas.coords(self.item)[0]
-        self.snake_y1 = Snake.my_canvas.coords(self.item)[1]
-        self.snake_x2 = Snake.my_canvas.coords(self.item)[2]
-        self.snake_y2 = Snake.my_canvas.coords(self.item)[3] 
+
 
         if self.snake_x1 > 350 or self.snake_x1 < 0 or self.snake_y1 > 350 or self.snake_y1 < 0:
             #*If I can go through the walls
             if self.wall_death:
                 #*Return to original position
-                Snake.my_canvas.move(self.item, -self.x, -self.y)
-                Snake.my_canvas.after_cancel(self.alive)
+                Snake.my_canvas.move(self.head, -self.x, -self.y)
+                self.after_var = False
             else:
+                #*Could make used moveto()
                 if self.snake_x1 > 350:
-                    Snake.my_canvas.move(self.item, -375, 0)
+                    Snake.my_canvas.move(self.head, -375, 0)
                 elif self.snake_x1 < 0:
-                    Snake.my_canvas.move(self.item, 375, 0)
+                    Snake.my_canvas.move(self.head, 375, 0)
                 elif self.snake_y1 > 350:
-                    Snake.my_canvas.move(self.item, 0, -375)
+                    Snake.my_canvas.move(self.head, 0, -375)
                 elif self.snake_y1 < 0:
-                    Snake.my_canvas.move(self.item, 0, 375)
-                self.alive = self.my_canvas.after(200, self.movement)
+                    Snake.my_canvas.move(self.head, 0, 375)
             
 
+        if self.after_var:
+            self.alive = Snake.my_canvas.after(1000, self.movement)
         else:
-            self.alive = Snake.my_canvas.after(200, self.movement)
+            Snake.my_canvas.after_cancel(self.alive)
+
+        #*Storing direction to see if there was a turn
+        self.last_direction = self.snake_direction
+        print(f"snake direction is {self.snake_direction}")
+        print(f"last direction is {self.last_direction}")
+
+    def add_length(self):
+        #*New rectangles will spawn based on which direction the snake was travelling last
+        if self.snake_direction == 'up':
+            self.add_length_x1 = self.snake_x1
+            #*Careful with the coordinate system
+            self.add_length_y1 = self.snake_y1 + 25
+            self.add_length_x2 = self.snake_x2
+            self.add_length_y2 = self.snake_y2 + 25
+
+        elif self.snake_direction == 'down':
+            self.add_length_x1 = self.snake_x1
+            self.add_length_y1 = self.snake_y1 + 25
+            self.add_length_x2 = self.snake_x2
+            self.add_length_y2 = self.snake_y2 + 25
+
+        elif self.snake_direction == 'left':
+            self.add_length_x1 = self.snake_x1 + 25
+            self.add_length_y1 = self.snake_y1
+            self.add_length_x2 = self.snake_x2 + 25
+            self.add_length_y2 = self.snake_y2
+
+        elif self.snake_direction == 'right':
+            self.add_length_x1 = self.snake_x1 - 25
+            self.add_length_y1 = self.snake_y1
+            self.add_length_x2 = self.snake_x2 - 25
+            self.add_length_y2 = self.snake_y2 
+
+      #*As long as the rectange is in the list, I don't think I have to name it.
+        self.snake_body.append(Snake.my_canvas.create_rectangle(self.add_length_x1,self.add_length_y1,self.add_length_x2, self.add_length_y2, fill='green'))
+        
 
 
 
@@ -77,21 +176,23 @@ class Snake():
     def up(self, event):
         self.x = 0
         self.y = -25
-
+        self.snake_direction = 'up'
 
     def down(self, event):
         self.x = 0
         self.y = 25
-
+        self.snake_direction = 'down'
 
     def left(self, event):
         self.x = -25
         self.y = 0
+        self.snake_direction = 'left'
 
 
     def right(self, event):
         self.x = 25
         self.y = 0
+        self.snake_direction = 'right'
 
 class Apple():
     apple_count = 0
@@ -106,12 +207,13 @@ class Apple():
         self.apple_x_list = self.apple_coord_list.copy()
         self.apple_y_list = self.apple_coord_list.copy()
 
+    def create_apple(self):
         self.apple_x1 = random.choice(self.apple_x_list)
         self.apple_x2 = self.apple_x1 + 25
         self.apple_y1 = random.choice(self.apple_y_list)
         self.apple_y2 = self.apple_y1 + 25
         Apple.apple_1 = Snake.my_canvas.create_rectangle(self.apple_x1, self.apple_y1, self.apple_x2, self.apple_y2, fill='red')
-
+        Apple.apple_count += 1
 
 
 
@@ -119,5 +221,6 @@ class Apple():
 
 a = Snake()
 b = Apple()
+b.create_apple()
 a.movement()
 mainloop()
---
+++ try.py
@@ -0,0 +1,4 @@
+a ='hi'
+b = a
+a = 'yeet'
+print(a, b)
